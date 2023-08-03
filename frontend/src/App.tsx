bd144aab6aacdf40b8bc41ceed4240e0b134966d
Brian Liu <bwliu@outlook.com>
2023-08-03 00:25:51 -0700

    Frontend Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Container } from '@mui/material';
-import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Container disableGutters>
-        <Header></Header>
+      <Container className="background-image" disableGutters>
+        <Footer></Footer>
       </Container>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Grid, Link } from '@mui/material';
+import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './../App.css';
+
+const Footer: React.FC = () => {
+    return (
+        <AppBar 
+            position="absolute" 
+            sx={{ 
+                width: '100%', 
+                backgroundColor: 'rgb(10,0,10,0.75)', 
+                top: 'auto',
+                bottom: '0',
+            }}
+        >
+            <Toolbar disableGutters>
+                <SettingsBackupRestoreIcon sx={{ ml: '3%', mr: '1%' }} />
+                <Typography variant="h6" sx={{ color: 'offwhite' }}>
+                    Reverse Prompt Engineer
+                </Typography>
+                <Grid container justifyContent="flex-end" alignItems="center" spacing={1} marginRight={'3%'}>
+                    <Grid item>
+                        <Typography variant="body1" sx={{ color: 'white', marginRight: '1rem' }}>
+                            Created by Brian Liu
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Link href="https://github.com/brianliu-ucsd" color="inherit">
+                            <GitHubIcon />
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link href="https://www.linkedin.com/in/brian-liu-ucsd" color="inherit">
+                            <LinkedInIcon />
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Footer;
